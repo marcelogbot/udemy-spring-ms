f2f0a70fd24926bf82c7766fa14a9520b3a59442
Marcelo G Botelho <marcunb@gmail.com>
2022-11-10 13:34:38 -0300

    Project hr-worked created

---
+++ hr-worker/src/test/java/com/cursoudemy/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
