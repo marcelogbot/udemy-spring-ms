37f911eaff50a46fc5561dc1fa80b9cad7a1598e
Marcelo G Botelho <marcunb@gmail.com>
2022-11-11 09:43:30 -0300

    FeignClient

--- hr-payroll/src/main/java/com/cursoudemy/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/cursoudemy/hrpayroll/service/PaymentService.java
@@ -1,33 +1,32 @@
 package com.cursoudemy.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.cursoudemy.hrpayroll.feignClients.WorkerFeingClient;
 import com.cursoudemy.hrpayroll.model.Payment;
 import com.cursoudemy.hrpayroll.model.Worker;
 
-@Service
+import lombok.extern.slf4j.Slf4j;
+
+@Service @Slf4j
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeingClient workerFeingClient;
 
     public Payment getPayment(Long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("workerId",workerId.toString());
-        Worker worker = restTemplate.getForObject(workerHost+"/worker/{workerId}",Worker.class, uriVariables);
+        Worker worker = workerFeingClient.findById(workerId).getBody();
+        log.info("Worker: {}",worker);
         
-
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        if(worker != null) {
+            return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        } else {
+            log.info("Worker nao encontrado.");
+            return null;
+        }
+        
     }
     
 }
