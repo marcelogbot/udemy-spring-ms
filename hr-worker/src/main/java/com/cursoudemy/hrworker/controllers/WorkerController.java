194e5e9ac9c3545bc3a44ad81eee2fe915ca05bc
Marcelo G Botelho <marcunb@gmail.com>
2022-11-12 12:30:13 -0300

    Actuator configured

--- hr-worker/src/main/java/com/cursoudemy/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/cursoudemy/hrworker/controllers/WorkerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,9 @@ import com.cursoudemy.hrworker.services.WorkerServices;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @RestController @Slf4j
-@RequestMapping(path = "worker")
+@RequestMapping(path = "/worker")
 public class WorkerController {
     
     @Value("${test.config}")
